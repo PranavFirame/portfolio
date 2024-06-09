9043c43414dd921301603a0d30a510e24c433fa3
Pranav Firame <pranavfirame06@gmail.com>
2024-06-10 02:41:02 +0530

    all website done just last touch up going on

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,15 +36,75 @@ const shadowHeader = ()=>{
 window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+      contactMessage = document.getElementById("contact-message");
+const submitButton = document.getElementById("submitbtn");
 
+
+const sendEmail = (e)=>{
+    e.preventDefault()
+    emailjs.sendForm("service_tj9jjbm","template_w072z27","#contact-form","mC4HO-tv044EHRBb0").then(()=>{
+        contactMessage.textContent = "Message sent successfully ✅";
+        setTimeout(()=>{
+            contactMessage.textContent = "";
+        }, 5000);
+        contactForm.reset();
+    })
+
+}
+
+contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = ()=>{
+    const scrollUp = document.getElementById("scroll-up");
+    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll"): scrollUp.classList.remove("show-scroll");
+}
 
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = ()=>{
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute("id");
+        sectionClass= document.querySelector(".nav__menu a[href*=" + sectionId+ "]");
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add("active-link");
+        }else{
+            sectionClass.classList.remove("active-link");
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive);
 
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = ()=> document.body.classList.contains("darkTheme")?"dark":"light";
+const getCurrentIcon = ()=> themeButton.classList.contains(iconTheme)?"ri-moon-line" : "ri-sun-line";
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === "ri-moon-line"? 'add':'remove'](iconTheme);
+}
+
+themeButton.addEventListener("click", ()=>{
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
